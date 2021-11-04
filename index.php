5bc74bd7c8ea9d6e4c224ad5d4c38b65be3ded92
DD <dedy.darmawan@global.ntt>
2021-11-04 19:26:58 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Belajar PHP" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "saya sedang belajar PHP<br>";
+            echo "<p>Belajar PHP hingga jadi master</p>";
+        ?>
+        </br>
+        <?php
+            $Nama = "Dedy";
+            $Umur = "13";
+            echo "Nama saya adalah $Nama";
+            echo "</br>";
+
+            if ($Umur >= 13){
+                echo "Kamu sudah dewasa";
+                echo "</br>";
+                echo "Selamat datangaa";
+                echo "</br>";
+                echo "Kamu boleh minum ssskopi";
+            }
+        ?>
+    </body>
+</html>
